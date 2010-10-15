444e46587c0da4d38ae9323b260c52c6673f91d5
Luis Useche <luis@cs.fiu.edu>
2010-10-14 23:21:30 -0400

    Fixed ata_piix reader. When an issue event is read, check if the number of blocks are greater than 0.

--- trace_reader/trace_ata_piix.c
+++ trace_reader/trace_ata_piix.c
@@ -1,20 +1,23 @@
 #include <glib.h>
 #include <assert.h>
+#include <string.h>
 
 #include <trace.h>
 
+#include <blktrace.h>
 #include <blktrace_api.h>
 
 static struct blk_io_trace at;
-static gboolean at_used = FALSE, send_p_d = FALSE;
+static gboolean send_p_d = FALSE;
 static int out_reqs = 0;
+static __u64 blks;
 
 gboolean trace_ata_piix_read_next(const struct trace *dt, struct blk_io_trace *t) {
 	gboolean r;
 
 	if(send_p_d) {
 		/* if we have a pending D event,
-		 * we first send this before than the next one 
+		 * we first send this before the next one
 		 */
 		*t = at;
 		send_p_d = FALSE;
@@ -22,6 +25,7 @@ gboolean trace_ata_piix_read_next(const struct trace *dt, struct blk_io_trace *t
 	} else {
 reread:
 		r = trace_read_next(dt,t);
+		blks = t_blks(t);
 
 		/* if the trace reach the end */
 		if(!r) return FALSE;
@@ -41,6 +45,8 @@ reread:
 				break;
 
 			case __BLK_TA_ISSUE:
+				if(blks==0)
+					break;
 				
 				/* we should not have more than 2 
 				 * outstanding rqs for this driver 
@@ -49,7 +55,6 @@ reread:
 
 				if(out_reqs++==1) {
 					at = *t;
-					at_used = TRUE;
 
 					/* after this, we need to reread
 					 * the next trace entry 
@@ -59,7 +64,6 @@ reread:
 				break;
 
 			case __BLK_TA_REQUEUE:
-				at_used = FALSE;
 				out_reqs -= out_reqs>0?1:0;
 				break;
 		}
